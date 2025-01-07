e757bc562ddd4933858b5a605cd9f722546f546d
ducdove0312 <141986312+ducdo0312@users.noreply.github.com>
2025-01-08 00:20:12 +0700

    Add files via upload

---
+++ java/com/example/springsehibernate/SpringSeHibernateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springsehibernate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSeHibernateApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
